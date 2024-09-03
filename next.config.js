9fb3fb5a97fda71c5a7b6bfccfbc8977b87f97e8
gowda64 <gowdavishwas73@gmail.com>
2024-09-03 15:08:16 +0530

    added code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/my-portfolio' : '',
+    assetPrefix: isProd ? '/my-portfolio/' : '',
 };
 
 export default nextConfig;
